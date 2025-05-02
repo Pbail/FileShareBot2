8df4456f789623f63664cbf570b083e95455b452
Pbail Squad <156425176+Pbail@users.noreply.github.com>
2025-05-03 01:44:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,10 +22,10 @@ API_HASH = os.environ.get("API_HASH", "")
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002587062298"))
 
 # NAMA OWNER
-OWNER = os.environ.get("OWNER", "VeldXd")
+OWNER = os.environ.get("OWNER", "Miss_India_Here")
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "6497757690"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7813704601"))
 
 #Port
 PORT = os.environ.get("PORT", "8030")
